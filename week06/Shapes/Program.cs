b7946acbd4479309a93b513541934a1b9e8f6f0b
dember-byu <dember93hide@gmail.com>
2026-06-11 19:40:41 -0400

    ok

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,32 @@
+
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        
+        List<Shape> shapes = new List<Shape>();
+
+        
+        Square square1 = new Square("Red", 4);
+        Rectangle rectangle1 = new Rectangle("Blue", 5, 3);
+        Circle circle1 = new Circle("Green", 2.5);
+
+        
+        shapes.Add(square1);
+        shapes.Add(rectangle1);
+        shapes.Add(circle1);
+
+        
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            
+            Console.WriteLine($"The {color} shape has an area of: {area:F2}");
+        }
     }
 }
