55d0177d5b54239f82e49c794f5406bdf413b31d
dember-byu <dember93hide@gmail.com>
2026-06-16 17:05:24 -0400

    ok

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+namespace ExerciseTracking
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        static void Main(string[] args)
+        {
+            
+            List<Activity> activities = new List<Activity>();
+
+            
+            Running runningActivity = new Running("03 Nov 2022", 30, 3.0);
+            Cycling cyclingActivity = new Cycling("03 Nov 2022", 30, 12.0);
+            Swimming swimmingActivity = new Swimming("03 Nov 2022", 30, 20);
+
+            
+            activities.Add(runningActivity);
+            activities.Add(cyclingActivity);
+            activities.Add(swimmingActivity);
+
+            
+            foreach (Activity activity in activities)
+            {
+                Console.WriteLine(activity.GetSummary());
+            }
+        }
     }
 }
